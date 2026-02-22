2f768efd7a7feef221c3b681066c52343846da41
JJANGSOON <invs07@gmail.com>
2026-02-22 19:10:14 +0900

    chore(analytics): enable GA debug mode for validation

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${GA_ID}');
+                gtag('config', '${GA_ID}', { debug_mode: true });
               `}
             </Script>
           </>
